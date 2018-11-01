f3b87b557c0c5413b50b9862b4d6003f6e37dd59
Nick <nickmly@gmail.com>
2018-11-01 15:06:38 -0400

    Merged ChangeTrackingHook

--- ChangeTracking03/ChangeTrackingHook.cs
+++ ChangeTracking03/ChangeTrackingHook.cs
@@ -15,9 +15,42 @@ using Microsoft.Azure.Documents;
 using System.Data.SqlClient;
 using System.Data;
 using System.Net.Http.Headers;
+using System.Text;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace ChangeTracking03
 {
+    public class Fact
+    {
+        public string value { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Section
+    {
+        public IList<Fact> facts { get; set; }
+    }
+
+    public class ApplicationChange
+    {
+        public string title { get; set; }
+        public IList<Section> sections { get; set; }
+        public string text { get; set; }
+    }
+
+    public class ApplicationChangeDbRecord
+    {
+        public string Date;
+        public string StartedBy;
+        public string TargetCloud;
+        public string DeploymentKey;
+        public string Reason;
+        public string Status;
+    }
+
+
     public static class ChangeTrackingHook
     {
         private static HttpClient httpClient = new HttpClient();
@@ -27,6 +60,9 @@ namespace ChangeTracking03
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+            int nProcessID = Process.GetCurrentProcess().Id;
+            log.LogInformation($"ProcessID [{nProcessID}]");
+
             var keyVaultName = Environment.GetEnvironmentVariable("KEY_VAULT_NAME");
             var keyVaultUrl = $"https://{keyVaultName}.vault.azure.net/";
 
@@ -41,9 +77,7 @@ namespace ChangeTracking03
             var azureServiceTokenProvider = new AzureServiceTokenProvider();
             var kvClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback), httpClient);
 
-
             // Read Application configuration from database
-//            string sqlConnectionString = (await kvClient.GetSecretAsync(keyVaultUrl, Environment.GetEnvironmentVariable("SQLMGT_CONNECTION_STRING"))).Value;
             string sqlConnectionString = (await kvClient.GetSecretAsync(keyVaultUrl, Environment.GetEnvironmentVariable("LOCALSQL_CONNECTION_STRING"))).Value;
             log.LogInformation($"SQL connection string. [{sqlConnectionString}]");
             string appChangeWebHookURL = "No appChangeWebHookURL";
@@ -52,9 +86,13 @@ namespace ChangeTracking03
                 SqlCommand cmd = new SqlCommand();
                 SqlDataReader reader;
 
-                cmd.CommandText = "SELECT [WebHookUrl] FROM[dbo].[GoCLoudApplicationWithHooks] where clientapplicationname = 'EIC'";
+                cmd.CommandText = "SELECT [WebHookUrl] FROM[dbo].[GoCLoudApplicationWithHooks] where clientapplicationname = 'MSC'";
                 cmd.CommandType = CommandType.Text;
                 cmd.Connection = connection;
+
+                // waiting for .net Core to be supported to use MSI for SQL
+                // string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync(https://database.windows.net/);
+                // connection.AccessToken = accessToken;
 
                 connection.Open();
                 reader = cmd.ExecuteReader();
@@ -70,18 +108,81 @@ namespace ChangeTracking03
                 connection.Close();
             }
 
+            string sqlMgtConnectionString = (await kvClient.GetSecretAsync(keyVaultUrl, Environment.GetEnvironmentVariable("SQLMGT_CONNECTION_STRING"))).Value;
+            string cmdString = "INSERT INTO [dbo].[GoCloudChanges],[ChangeID],[ChangeTargetType],[TargetCloud],[Summary],[Description],[State],[Status],[StatusReason],[EnvironmentRecordId],[RiskLevel],[OutageMinutes],[ProjectID],[Class],[AuthorUpn],[ApproverUpn] VALUES(@val1, @va2, @val3)";
+            using (SqlConnection conn = new SqlConnection(sqlMgtConnectionString))
+            {
+                using (SqlCommand comm = new SqlCommand())
+                {
+                    comm.Connection = conn;
+                    comm.CommandText = cmdString;
+                    comm.Parameters.AddWithValue("@val1", txtbox1.Text);
+                    comm.Parameters.AddWithValue("@val2", txtbox2.Text);
+                    comm.Parameters.AddWithValue("@val3", txtbox3.Text);
+                    conn.Open();
+                    comm.ExecuteNonQuery();
+                }
+            }
+
             string dbConnectionString = (await kvClient.GetSecretAsync(keyVaultUrl, Environment.GetEnvironmentVariable("DB_CONNECTION_STRING"))).Value;
             string dbAccessKey = (await kvClient.GetSecretAsync(keyVaultUrl, Environment.GetEnvironmentVariable("DB_ACCESS_KEY"))).Value;
             log.LogInformation($"DB connection string. [{dbConnectionString}]");
+            var client = new DocumentClient(new Uri(dbConnectionString), dbAccessKey);
+
+            // Get Change tracking metadata
+            var trackedChange = JsonConvert.DeserializeObject<ApplicationChange>(requestBody);
+            var dbChangeRecord = new ApplicationChangeDbRecord();
+            Type dbChangeRecordType = dbChangeRecord.GetType();
+            var facts = trackedChange.sections[0].facts;
+            Fact datefact = new Fact();
+            datefact.name = "Date";
+            datefact.value = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssK");
+            facts.Add(datefact);
+            FieldInfo[] Fields = dbChangeRecordType.GetFields();
+
+            for (int i = 0; i < facts.Count; i++)
+            {
+                Fact fact = facts[i];
+                string propertyName = fact.name.Replace(":", String.Empty);
+                log.LogInformation($"CHECKING: {propertyName}");
+                FieldInfo dbRecordFieldInfo = dbChangeRecordType.GetField(propertyName);
+                if (dbRecordFieldInfo != null)
+                {
+                    log.LogInformation($"Setting: {propertyName}");
+                    dbRecordFieldInfo.SetValue(dbChangeRecord, fact.value);
+                }
+            }
+
+            for (int i = 0; i < facts.Count; i++)
+            {
+                Fact fact = facts[i];
+                string propertyName = fact.name.Replace(":", String.Empty);
+                log.LogInformation($"CHECKING: {propertyName}");
+                if (propertyName.Equals("changeJobURL"))
+                {
+                    facts.RemoveAt(i);
+                    break;
+                }
+            }
+
+
+            // Send webhook notification to teams
+            //            var content = new StringContent(JsonConvert.SerializeObject(new { title = "Azure Hook Test", Text = "Some cool app changes" }), Encoding.UTF8, "application/json");
+            string jsonString = JsonConvert.SerializeObject(trackedChange);
+            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            log.LogInformation($"DEBUG: {trackedChange}");
+            log.LogInformation($"DEBUG OUTPUT: {jsonString}");
+            var response = await httpClient.PostAsync(appChangeWebHookURL, content);
+            if (response.IsSuccessStatusCode)
+            {
+                //do what needs to be done
+            }
 
             //////////////
             // CosmosDB
             //////////////
-            var client = new DocumentClient(new Uri(dbConnectionString), dbAccessKey);
-            Document doc = await client.UpsertDocumentAsync(UriFactory.CreateDocumentCollectionUri("ActivityLogDb", "AppChangeTracking"), 
-                new { Date = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssK"), StartedBy = $"{data?.StartedBy}", TargetCloud = $"{data?.TargetCloud}",
-                    DeploymentKey = $"{data?.DeploymentKey}", Reason = $"{data?.Reason}", Status = $"{data?.Status}",
-                    appChangeWebHookURL = $"{appChangeWebHookURL}", sqlConn=$"{sqlConnectionString}" });
+            Document doc = await client.UpsertDocumentAsync(UriFactory.CreateDocumentCollectionUri("ActivityLogDb", "AppChangeTracking"),
+                trackedChange);
             //////////////
             //////////////
 
@@ -110,8 +211,9 @@ namespace ChangeTracking03
             }
             //////////////
             //////////////
+
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name} {dbConnectionString} {requestBody}")
+                ? (ActionResult)new OkObjectResult($"Hello, {name} {dbConnectionString} {trackedChange} {requestBody}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
